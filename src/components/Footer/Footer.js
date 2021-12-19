df41cc5ed46cdd96ce472dc567bb00f87ecec286
pinkibhardwaj <93024170+pinkibhardwaj@users.noreply.github.com>
2021-12-19 21:41:30 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.linkedin.com/in/pinki-bhardwaj-5279b51b9/"
+        style={{ cursor: "pointer" }}
+      >
+       Pinki bhardwaj
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
